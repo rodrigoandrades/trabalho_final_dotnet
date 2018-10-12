ec4b44eec53e180a32f51f5281ca6ff66f336fa4
Rodrigo Andrades <rodrigomail2007@gmail.com>
2018-10-11 17:10:07 -0700

    first commit

---
+++ TrabalhoFinal.Net/Notification.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TrabalhoFinal.Net
+{
+    public class Notification : INotification
+    {
+        public string GetMessage(string message)
+        {
+            return string.Format("You entered: {0}", message);
+        }
+    }
+}
